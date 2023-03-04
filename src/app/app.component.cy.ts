4a1c4f37fae493474af553db77c851335bb09cff
Marcel Hoegel <github@mclhgl.de>
2023-03-04 21:45:27 +0100

    Write test for AppComponent; Remove unused tsconfigs

--- src/app/app.component.cy.ts
+++ src/app/app.component.cy.ts
@@ -1,7 +1,14 @@
 import {AppComponent} from './app.component';
 
 describe('AppComponent', () => {
-    it('mounts', () => {
+    it('should be created', () => {
         cy.mount(AppComponent);
+        cy.get('div').should('have.class', 'app');
+        cy.get('router-outlet').should('exist');
+    });
+
+    it('should contain router-outlet', () => {
+        cy.mount(AppComponent);
+        cy.get('div > router-outlet').should('exist');
     });
 });
