cca1fa954e738306a57504bd601a37880b26c4cb
Marcel Hoegel <github@mclhgl.de>
2023-03-05 13:53:36 +0100

    Install jasmine, spectator; add karma config; Write app component test

---
+++ src/app/app.component.spec.ts
@@ -0,0 +1,18 @@
+
+import {AppComponent} from './app.component';
+import {Spectator, createComponentFactory, SpectatorFactory} from '@ngneat/spectator';
+
+describe('AppComponent', () => {
+    let spectator: Spectator<AppComponent>;
+    const createComponent: SpectatorFactory<AppComponent> = createComponentFactory(AppComponent);
+
+    beforeEach(() => spectator = createComponent());
+
+    it('should create the app', () => {
+        expect(spectator.query('div')).toHaveClass('app');
+    });
+
+    it('should contain router-outlet', () => {
+        expect(spectator.query('div.app')).toHaveDescendant('router-outlet');
+    });
+});
