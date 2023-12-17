b296d9888640866a949e6e7c79fd421e5cdc5bca
Marcel Hoegel <marcel.hoegel@virtualminds.com>
2023-12-17 22:59:49 +0100

    reactivate tests

--- src/app/home/home.component.spec.ts
+++ src/app/home/home.component.spec.ts
@@ -8,6 +8,7 @@ import {of} from 'rxjs';
 import {ShareDialogComponent} from '../share-dialog/share-dialog.component';
 import {CreationDialogComponent} from '../creation-dialog/creation-dialog.component';
 import {FinishDialogComponent} from '../finish-dialog/finish-dialog.component';
+import {Difficulty} from "fake-sudoku-puzzle-generator";
 
 describe('HomeComponent', () => {
     const grid: SudokuGrid = [
@@ -49,7 +50,16 @@ describe('HomeComponent', () => {
         shallow: true,
     });
 
-    beforeEach(() => spectator = createComponent());
+    beforeEach(() => {
+        spectator = createComponent();
+        jest.useFakeTimers({
+            legacyFakeTimers: true,
+        });
+    });
+
+    afterEach(() => {
+        jest.useRealTimers();
+    });
 
     it('should create component', () => {
         expect(spectator.query('div')).toHaveClass('home');
@@ -59,47 +69,47 @@ describe('HomeComponent', () => {
         expect(spectator.query('div.home')).toHaveDescendant('grid.home__grid');
     });
 
-    // it('should open share dialog', () => {
-    //     spectator.inject(MatDialog).open.andReturn({afterClosed: () => of(void 0)});
-    //     spectator.component.openShareDialog(grid);
-    //     expect(spectator.inject(MatDialog).open).toHaveBeenCalledWith(
-    //         SudokuShareDialogComponent,
-    //         {data: {grid}},
-    //     );
-    // });
+    it('should open share dialog', () => {
+        spectator.inject(MatDialog).open.andReturn({afterClosed: () => of(void 0)});
+        spectator.component.openShareDialog(grid);
+        expect(spectator.inject(MatDialog).open).toHaveBeenCalledWith(
+            ShareDialogComponent,
+            {data: {grid}},
+        );
+    });
 
-    // it('should open creation dialog', () => {
-    //     const difficulty: Difficulty = 'Easy';
-    //     const createRandomSudokuSpy: jest.SpyInstance = jest.spyOn(spectator.component, 'createRandomSudoku');
-    //     spectator.inject(MatDialog).open.andReturn({afterClosed: () => of(difficulty)});
-    //     spectator.component.openCreationDialog();
-    //     expect(spectator.inject(MatDialog).open).toHaveBeenCalledWith(
-    //         CreationDialogComponent,
-    //         {data: {}},
-    //     );
-    //     expect(createRandomSudokuSpy).toHaveBeenCalledWith(difficulty);
-    // });
+    it('should open creation dialog', () => {
+        const difficulty: Difficulty = 'Easy';
+        const createRandomSudokuSpy: jest.SpyInstance = jest.spyOn(spectator.component, 'createRandomSudoku');
+        spectator.inject(MatDialog).open.andReturn({afterClosed: () => of(difficulty)});
+        spectator.component.openCreationDialog();
+        expect(spectator.inject(MatDialog).open).toHaveBeenCalledWith(
+            CreationDialogComponent,
+            {data: {}},
+        );
+        expect(createRandomSudokuSpy).toHaveBeenCalledWith(difficulty);
+    });
 
-    // it('should open finish dialog with solved parameters', (() => {
-    //     spectator.inject(MatDialog).open.andReturn({afterClosed: () => of(void 0)});
-    //     spectator.detectChanges();
-    //     spectator.component.openFinishDialog({
-    //         grid,
-    //         isGridValid: true,
-    //         time: 0,
-    //     });
-    //     spectator.detectChanges();
-    //     expect(spectator.inject(MatDialog).open).toHaveBeenCalledWith(
-    //         FinishDialogComponent,
-    //         {
-    //             data: {
-    //                 title: 'Skrrr skrrr',
-    //                 description: 'You solved the puzzle in 00:00:00',
-    //                 icon: 'sentiment_very_satisfied',
-    //             },
-    //         },
-    //     );
-    // }));
+    it('should open finish dialog with solved parameters', (() => {
+        spectator.inject(MatDialog).open.andReturn({afterClosed: () => of(void 0)});
+        spectator.detectChanges();
+        spectator.component.openFinishDialog({
+            grid,
+            isGridValid: true,
+            time: 0,
+        });
+        spectator.detectChanges();
+        expect(spectator.inject(MatDialog).open).toHaveBeenCalledWith(
+            FinishDialogComponent,
+            {
+                data: {
+                    title: 'Skrrr skrrr',
+                    description: 'You solved the puzzle in 00:00:00',
+                    icon: 'sentiment_very_satisfied',
+                },
+            },
+        );
+    }));
 
     it('should open finish dialog with unsolved parameters', () => {
         spectator.inject(MatDialog).open.andReturn({afterClosed: () => of(void 0)});
