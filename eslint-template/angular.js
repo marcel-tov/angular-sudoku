9b0c64d59e96b6368ffb60516762fb35dbb1d5ca
Marcel Hoegel <marcel.hoegel@virtualminds.com>
2023-12-24 23:09:39 +0100

    update lint configs

--- eslint-template/angular.js
+++ eslint-template/angular.js
@@ -5,70 +5,76 @@ module.exports = {
     env: {
         es6: true,
     },
-    overrides: [{
-        // Typescript project files
-        files: [
-            'src/**/*.ts',
-        ],
-        parserOptions: {
-            project: 'tsconfig.eslint.json',
+    overrides: [
+        {
+            // Typescript project files
+            files: [
+                'src/**/*.ts',
+            ],
+            parserOptions: {
+                project: 'tsconfig.eslint.json',
+            },
+            extends: [
+                '../eslint-config/typescript',
+                '../eslint-config/angular',
+            ],
+            overrides: [{
+                files: '**/*.spec.ts',
+                extends: '../eslint-config/jest',
+            }],
         },
-        extends: [
-            '../eslint-config/typescript',
-            '../eslint-config/angular',
-        ],
-        overrides: [{
-            files: '**/*.spec.ts',
-            extends: '../eslint-config/jest',
-        }],
-    }, {
-        // Typescript project files
-        files: [
-            'src/**/*.ts',
-        ],
-        parserOptions: {
-            project: 'tsconfig.cypress.json',
+        {
+            // Typescript project files
+            files: [
+                'src/**/*.ts',
+            ],
+            parserOptions: {
+                project: 'tsconfig.eslint.json',
+            },
+            extends: [
+                '../eslint-config/typescript',
+                '../eslint-config/angular',
+            ],
+            overrides: [{
+                files: '**/*.cy.ts',
+                extends: '../eslint-config/cypress',
+            }],
         },
-        extends: [
-            '../eslint-config/typescript',
-            '../eslint-config/angular',
-        ],
-        overrides: [{
-            files: '**/*.cy.ts',
-            extends: '../eslint-config/cypress',
-        }],
-    }, {
-        // Typescript root files
-        files: '**/*.ts',
-        excludedFiles: [
-            'src/**/*.ts',
-        ],
-        parserOptions: {
-            project: 'tsconfig.node.json',
+        {
+            // Typescript root files
+            files: '**/*.ts',
+            excludedFiles: [
+                'src/**/*.ts',
+            ],
+            parserOptions: {
+                project: 'tsconfig.node.json',
+            },
+            extends: [
+                'eslint:recommended',
+                'plugin:@typescript-eslint/recommended-requiring-type-checking',
+                '../eslint-config/typescript',
+                '../eslint-config/node',
+            ],
         },
-        extends: [
-            'eslint:recommended',
-            'plugin:@typescript-eslint/recommended-requiring-type-checking',
-            '../eslint-config/typescript',
-            '../eslint-config/node',
-        ],
-    }, {
-        // Javascript root files
-        files: '**/*.js',
-        excludedFiles: [
-            './src/**/*.js',
-        ],
-        env: {
-            commonjs: true,
+        {
+            // Javascript root files
+            files: '**/*.js',
+            excludedFiles: [
+                './src/**/*.js',
+            ],
+            env: {
+                commonjs: true,
+            },
+            extends: [
+                'eslint:recommended',
+                '../eslint-config/javascript',
+                '../eslint-config/node',
+            ],
         },
-        extends: [
-            'eslint:recommended',
-            '../eslint-config/javascript',
-            '../eslint-config/node',
-        ],
-    }, {
-        // Angular templates
-        files: '**/*.html',
-        extends: '../eslint-config/angular-template',
-    }],
+        {
+            // Angular templates
+            files: '**/*.html',
+            extends: '../eslint-config/angular-template',
+        },
+    ],
 };
