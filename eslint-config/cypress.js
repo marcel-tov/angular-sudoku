f80e01be283080baa1d7247428cd1d95b8a5d5a8
Marcel Hoegel <marcel.hoegel@virtualminds.com>
2023-12-28 22:29:57 +0100

    clean up

--- eslint-config/cypress.js
+++ eslint-config/cypress.js
@@ -1,10 +1,6 @@
 /** Used for all .spec.ts files. */
 module.exports = {
-    // env: {
-    //     jasmine: true,
-    // },
     extends: [
         'plugin:cypress/recommended',
-        // './spec',
     ],
 };
