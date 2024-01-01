728512f0ca082afc703d238d0531d115c6165ad6
marcel-tov <anchors_hooves_0n@icloud.com>
2024-01-01 16:49:56 +0100

    Fix lint error

--- cypress/e2e/page-not-found.cy.ts
+++ cypress/e2e/page-not-found.cy.ts
@@ -1,13 +1,13 @@
 describe('page-not-found', () => {
-  it('Should contain page-not-found tag', () => {
-    cy.visit(`${Cypress.config().baseUrl}/foo/bar`);
-    cy.get('page-not-found').should('exist');
-  })
+    it('Should contain page-not-found tag', () => {
+        cy.visit(`${Cypress.config().baseUrl}/foo/bar`);
+        cy.get('page-not-found').should('exist');
+    });
 
-  it('Should redirect to home page', () => {
-    cy.visit('/foo/bar');
-    cy.get('page-not-found').should('exist');
-    cy.get('.page-not-found-link__home-link').click();
-    cy.url().should('eq', `${Cypress.config().baseUrl}`);
-  })
-})
+    it('Should redirect to home page', () => {
+        cy.visit('/foo/bar');
+        cy.get('page-not-found').should('exist');
+        cy.get('.page-not-found-link__home-link').click();
+        cy.url().should('eq', `${Cypress.config().baseUrl}`);
+    });
+});
