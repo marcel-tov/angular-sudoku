43405c03937af6d621b75b87a67463b82f17eded
Marcel <anchors_hooves_0n@icloud.com>
2024-01-09 22:15:00 +0100

    Revert "Extract top navigation from grid (#23)"
    This reverts commit 3048a0662a46bf39e0d0a75e56fdb94d20c1f895.

--- src/app/core/grid/grid.component.cy.ts
+++ src/app/core/grid/grid.component.cy.ts
@@ -9,6 +9,51 @@ describe('GridComponent', () => {
         cy.get('div').should('have.class', 'grid');
     });
 
+    describe('top navigation', () => {
+        it('Does contain grid top navigation', () => {
+            mount(GridComponent, {componentProperties: {showTopNavigation: true, showFooterNavigation: false}});
+            cy.get('div.grid').should('have.descendants', 'div.grid__navigation');
+        });
+
+        it('Does not contain grid navigation', () => {
+            mount(GridComponent, {componentProperties: {showTopNavigation: false, showFooterNavigation: false}});
+            cy.get('div.grid').should('not.have.descendants', 'div.grid__navigation');
+        });
+
+        it('Does contain share button', () => {
+            mount(GridComponent);
+            cy.get('.grid__navigation button[aria-label="Share sudoku by url"]')
+                .should('have.descendants', 'mat-icon')
+                .and('have.text', 'share');
+        });
+
+        it('Does contain start new game button', () => {
+            mount(GridComponent);
+            cy.get('.grid__navigation button[aria-label="Start a new game"]')
+                .should('have.descendants', 'mat-icon')
+                .and('have.text', 'autorenew');
+        });
+
+        it('Does contain clear all button', () => {
+            mount(GridComponent);
+            cy.get('.grid__navigation button[aria-label="Make locked fields editable"]').click();
+            cy.get('.grid__navigation button[aria-label="Clear all values"]')
+                .should('have.descendants', 'mat-icon')
+                .and('have.text', 'clear_all');
+        });
+
+        it('Does contain lock button', () => {
+            mount(GridComponent);
+            cy.get('.grid__navigation button[aria-label="Make locked fields editable"]')
+                .should('have.descendants', 'mat-icon')
+                .and('have.text', 'lock');
+            cy.get('.grid__navigation button[aria-label="Make locked fields editable"]').click();
+            cy.get('.grid__navigation button[aria-label="Make locked fields editable"]')
+                .should('have.descendants', 'mat-icon')
+                .and('have.text', 'lock_open');
+        });
+    });
+
     describe('grid', () => {
         const grid: SudokuGrid = [
             getEmptyRow(),
