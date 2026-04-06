d8edf995e1a01ba9180671c8523b60b86397d564
Marcel <anchors_hooves_0n@icloud.com>
2026-04-06 21:04:40 +0200

    Update cypress.js
    eslint-plugin-cypress v3+ ships only a flat config format, which is incompatible with the legacy eslintrc extends syntax the project uses. The fix is to replace the extends with manual plugin/env setup:

--- eslint-config/cypress.js
+++ eslint-config/cypress.js
@@ -1,6 +1,16 @@
 /** Used for all .spec.ts files. */
 module.exports = {
-    extends: [
-        'plugin:cypress/recommended',
-    ],
+    plugins: ['cypress'],
+    env: {
+        'cypress/globals': true,
+    },
+    rules: {
+        'cypress/no-assigning-return-values': 'error',
+        'cypress/no-unnecessary-waiting': 'error',
+        'cypress/no-async-tests': 'error',
+        'cypress/no-async-before': 'error',
+        'cypress/no-pause': 'error',
+        'cypress/assertion-before-screenshot': 'warn',
+        'cypress/no-force': 'warn',
+    },
 };
