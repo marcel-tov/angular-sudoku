12e3181e0814ed23579e57695ebb8a3b9125c1a4
Marcel <anchors_hooves_0n@icloud.com>
2026-04-07 22:46:28 +0200

    fix lint + fix tests

--- eslint-config/cypress.js
+++ eslint-config/cypress.js
@@ -1,16 +1,6 @@
 /** Used for all .spec.ts files. */
 module.exports = {
-    plugins: ['cypress'],
-    env: {
-        'cypress/globals': true,
-    },
-    rules: {
-        'cypress/no-assigning-return-values': 'error',
-        'cypress/no-unnecessary-waiting': 'error',
-        'cypress/no-async-tests': 'error',
-        'cypress/no-async-before': 'error',
-        'cypress/no-pause': 'error',
-        'cypress/assertion-before-screenshot': 'warn',
-        'cypress/no-force': 'warn',
-    },
+    extends: [
+        'plugin:cypress/recommended',
+    ],
 };
