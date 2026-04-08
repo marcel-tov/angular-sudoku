2704711fcf6c9eb9b6656bcafe448ec43706ea1d
Marcel <anchors_hooves_0n@icloud.com>
2026-04-08 23:45:29 +0200

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,14 @@ module.exports = {
     preset: 'jest-preset-angular',
     setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
     modulePathIgnorePatterns: ['<rootDir>/eslint-config/'],
+    transform: {
+        '^.+\\.(ts|js|mjs|html|svg)$': [
+            'jest-preset-angular',
+            {
+                tsconfig: '<rootDir>/tsconfig.spec.json',
+                stringifyContentPathRegex: '\\.(html|svg)$',
+                isolatedModules: true,
+            },
+        ],
+    },
 };
