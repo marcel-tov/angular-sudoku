5d8da28e8fae5cbfdb1e75f33600a0969e506a0d
Marcel <marcel.hoegel@virtualminds.com>
2026-04-25 20:03:32 +0200

    fix test

--- src/app/core/grid-footer-navigation/grid-footer-navigation.component.spec.ts
+++ src/app/core/grid-footer-navigation/grid-footer-navigation.component.spec.ts
@@ -10,30 +10,26 @@ describe('GridFooterNavigationComponent', () => {
         shallow: true,
     });
 
-    beforeEach(() => {
-        spectator = createComponent();
-    });
-
     it('Does show container', () => {
-        spectator.setInput({hasSelectedValue: false, showNominees: false, isHelpEnabled: false});
+        spectator = createComponent({props: {hasSelectedValue: false, showNominees: false, isHelpEnabled: false}});
         spectator.detectChanges();
         expect(spectator.queryAll('div.grid-footer-navigation')[0]).toHaveClass('grid-footer-navigation');
     });
 
     it('Does disable delete button when no value is selected', () => {
-        spectator.setInput({hasSelectedValue: false, showNominees: false, isHelpEnabled: false});
+        spectator = createComponent({props: {hasSelectedValue: false, showNominees: false, isHelpEnabled: false}});
         spectator.detectChanges();
         expect(spectator.query('button[aria-label="Delete a selected value"]')).toBeDisabled();
     });
 
     it('Does enable delete button when value is selected', () => {
-        spectator.setInput({hasSelectedValue: true, showNominees: false, isHelpEnabled: false});
+        spectator = createComponent({props: {hasSelectedValue: true, showNominees: false, isHelpEnabled: false}});
         spectator.detectChanges();
         expect(spectator.query('button[aria-label="Delete a selected value"]')).not.toBeDisabled();
     });
 
     it('Does emit deleteValue on delete button click', () => {
-        spectator.setInput({hasSelectedValue: true, showNominees: false, isHelpEnabled: false});
+        spectator = createComponent({props: {hasSelectedValue: true, showNominees: false, isHelpEnabled: false}});
         const deleteSpy: jest.SpyInstance = jest.spyOn(spectator.component.deleteValue, 'emit');
         spectator.detectChanges();
         const selector: DOMSelector = byTextContent('Delete number', {selector: 'button'});
@@ -42,7 +38,7 @@ describe('GridFooterNavigationComponent', () => {
     });
 
     it('Does emit nomineeToggle on nominees button click', () => {
-        spectator.setInput({hasSelectedValue: false, showNominees: false, isHelpEnabled: false});
+        spectator = createComponent({props: {hasSelectedValue: false, showNominees: false, isHelpEnabled: false}});
         const nomineeSpy: jest.SpyInstance = jest.spyOn(spectator.component.nomineeToggle, 'emit');
         spectator.detectChanges();
         const selector: DOMSelector = byTextContent('Nominees', {selector: 'button'});
@@ -51,25 +47,25 @@ describe('GridFooterNavigationComponent', () => {
     });
 
     it('Does show delete button text', () => {
-        spectator.setInput({hasSelectedValue: false, showNominees: false, isHelpEnabled: false});
+        spectator = createComponent({props: {hasSelectedValue: false, showNominees: false, isHelpEnabled: false}});
         spectator.detectChanges();
         expect(spectator.query('button[aria-label="Delete a selected value"]')).toHaveText('Delete number');
     });
 
     it('Does show nominees button text', () => {
-        spectator.setInput({hasSelectedValue: false, showNominees: false, isHelpEnabled: false});
+        spectator = createComponent({props: {hasSelectedValue: false, showNominees: false, isHelpEnabled: false}});
         spectator.detectChanges();
         expect(spectator.query('button[aria-label="Toggle field edit nominees"]')).toHaveText('Nominees');
     });
 
     it('Does show help slide toggle', () => {
-        spectator.setInput({hasSelectedValue: false, showNominees: false, isHelpEnabled: false});
+        spectator = createComponent({props: {hasSelectedValue: false, showNominees: false, isHelpEnabled: false}});
         spectator.detectChanges();
         expect(spectator.query('mat-slide-toggle')).toHaveText('Help');
     });
 
     it('Does contain nominee values', () => {
-        spectator.setInput({hasSelectedValue: false, showNominees: false, isHelpEnabled: false});
+        spectator = createComponent({props: {hasSelectedValue: false, showNominees: false, isHelpEnabled: false}});
         spectator.detectChanges();
         expect(spectator.element).toHaveDescendant('nominee-values');
     });
